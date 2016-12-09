aece8d08d626249e8db4772a8a268053ad1b6258
Yong Bakos <ybakos@humanoriented.com>
2016-12-09 06:15:02 -0800

    Add main stub.

---
+++ main.c
@@ -0,0 +1,10 @@
+/* A Simple Wayland Client
+ *
+ * Follow the most simple path through the API to display a surface.
+ *
+ * An API exploration to help drive documentation.
+ */
+
+int main(int argc, char **argv) {
+	return 0;
+}
