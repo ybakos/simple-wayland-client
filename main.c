8ae21b6caa0be71f9186f5c6fbfb9c2afd093f40
Yong Bakos <ybakos@humanoriented.com>
2016-12-12 20:32:25 -0800

    Check for null display before obtaining registry.

--- main.c
+++ main.c
@@ -4,10 +4,18 @@
  *
  * An API exploration to help drive documentation.
  */
+#include <stdlib.h>
+#include <stdio.h>
 #include <wayland-client.h>
 
 int main(int argc, char **argv) {
 	struct wl_display *display = wl_display_connect(NULL);
-	struct wl_registry *registry = wl_display_get_registry(display);
-	return 0;
+	struct wl_registry *registry;
+	if (display) {
+		registry = wl_display_get_registry(display);
+	} else {
+		perror("Could not obtain display");
+		exit(EXIT_FAILURE);
+	}
+	return EXIT_SUCCESS;
 }
